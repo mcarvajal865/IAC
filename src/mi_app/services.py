733864ed8d811f93229886620110b41cc6f41696
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 21:20:31 -0500

    Obtener datos del JSON

--- src/mi_app/services.py
+++ src/mi_app/services.py
@@ -10,3 +10,7 @@ from .exceptions import(
 class IACService:
     def __init__(self, storage: StorageInterface) -> None:
         self._storage = storage
+
+    def _get_all_data(self) -> Dict[str, Any]:
+        """Para obtener todos los datos del JSON"""
+        return self._storage.load()
