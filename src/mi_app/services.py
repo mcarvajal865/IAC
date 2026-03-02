eee5d6d65ddef91356fec4bd182013faf4fd1cc4
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 21:21:00 -0500

    Busca empresa por ID

--- src/mi_app/services.py
+++ src/mi_app/services.py
@@ -14,3 +14,13 @@ class IACService:
     def _get_all_data(self) -> Dict[str, Any]:
         """Para obtener todos los datos del JSON"""
         return self._storage.load()
+
+    def _get_company(self, data: Dict, company_id: int ) -> Dict[str, Any]:
+        """Busca empresa por su ID, si no existe lanza error"""
+        companies = data.get("companies", [])
+
+        for company in companies:
+            if company["id"] == company_id:
+                return company
+
+        raise CompanyNotFoundError(f"La empresa con ID {company_id} no existe en el sistema")
