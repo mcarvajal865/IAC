37c33f7b67fca9cf6f93908fcf1243075886965b
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 22:40:56 -0500

    librerias

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from pathlib import Path
+import typer
+from rich.console import Console
+from rich.table import Table
 
+from src.iac.models import Company
+from src.iac.services import IACService
+from src.iac.storage import JSONStorage
