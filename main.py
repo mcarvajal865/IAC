a7226e9930652ff75c4f9e9ca94a75d802d43228
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 22:44:33 -0500

    variables

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from rich.table import Table
 from src.iac.models import Company
 from src.iac.services import IACService
 from src.iac.storage import JSONStorage
+
+app = typer.Typer()
+console = Console()
+
+"""Inicializar variables de almacenamiento y servicios"""
+storage = JSONStorage(Path("data/database.json"))
+service = IACService(storage)
