f858fd49ac44cbbabd1faf5cc6d77cf49ffe6668
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 21:13:36 -0500

    Protocol

--- src/mi_app/storage.py
+++ src/mi_app/storage.py
@@ -1,3 +1,9 @@
 from typing import Protocol, Dict, Any
 from pathlib import Path
 import json
+
+class StorageInterface(Protocol):
+    """Define el contrato de la capa Storage"""
+    def load(self) -> Dict[str, Any]: ...
+
+    def save(self, data: Dict[str, Any]) -> None: ...
