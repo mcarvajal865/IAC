a74d99e9ec08a81f7e41d0db884e5ac40b7f216c
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 21:14:21 -0500

    Clase y constructor

--- src/mi_app/storage.py
+++ src/mi_app/storage.py
@@ -7,3 +7,10 @@ class StorageInterface(Protocol):
     def load(self) -> Dict[str, Any]: ...
 
     def save(self, data: Dict[str, Any]) -> None: ...
+
+class JSONStorage:
+    """Implementación del almacenamiento en formato JSON, responsable de leer y escribir el archivo"""
+
+    def __init__(self, filepath: Path):
+        """Inicializa la ruta donde se almacenará el archivo JSON"""
+        self.filepath = filepath
