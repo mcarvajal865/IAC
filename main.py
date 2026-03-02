30744d2bcc27d478e1eb7fd745917f7465e3a021
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 22:48:34 -0500

    Documentación

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from pathlib import Path
-import typer
-from rich.console import Console
-from rich.table import Table
+from pathlib import Path #Maneja rutas de archivos de forma segura
+import typer #Crear CLI facilmente
+from rich.console import Console #Imprime bonito
+from rich.table import Table #Imprime tablas
 
 from src.iac.models import Company
 from src.iac.services import IACService
 from src.iac.storage import JSONStorage
 
-app = typer.Typer()
-console = Console()
+app = typer.Typer() #Crea aplicaion CLI
+console = Console() #Muestra tablas bonitas
 
 """Inicializar variables de almacenamiento y servicios"""
 storage = JSONStorage(Path("data/database.json"))
