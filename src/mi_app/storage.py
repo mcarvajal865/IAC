282d4f487ff2a05b025f59ea65c314b7e5699b92
Manuela0613 <martinezgilmanuela@gmail.com>
2026-03-01 21:14:59 -0500

    Carga datos

--- src/mi_app/storage.py
+++ src/mi_app/storage.py
@@ -14,3 +14,11 @@ class JSONStorage:
     def __init__(self, filepath: Path):
         """Inicializa la ruta donde se almacenará el archivo JSON"""
         self.filepath = filepath
+
+    def load(self) -> Dict[str, Any]:
+        """Carga los datos desde el archivo JSON, si el archivo no existe, retorna una estructura base"""
+        if not self.filepath.exists():
+            return {"companies": []}
+
+        with self.filepath.open("r", encoding = "utf-8") as file: #utf-8: permite usar caracteres especiales
+            return json.load(file)
